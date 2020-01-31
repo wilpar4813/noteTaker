8c1ce316412aff360d3699a4aa1c85a4ca7ba5ac
Tripp's Repo <tripp.parham@me.com>
2020-01-31 14:41:15 -0500

    Initial update. Delete is not working yet.

---
+++ Develop/app.js
@@ -0,0 +1,35 @@
+// EXPRESS CONFIGURATION
+// This sets up the basic properties for our express server
+// ==============================================================================
+const express = require("express");
+
+// Tells node that we are creating an "express" server
+var app = express();
+
+// Sets an initial port. We"ll use this later in our listener
+const PORT = process.env.PORT || 3000;
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
+// Sets up the Express app to handle data parsing
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ================================================================================
+// ROUTER
+// The below points our server to a series of "route" files.
+// These routes give our server a "map" of how to respond when users visit or request data from various URLs.
+// ================================================================================
+
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER
+// The below code effectively "starts" our server
+// =============================================================================
+
+
---
+++ Develop/routes/apiRoutes.js
@@ -0,0 +1,75 @@
+// ===============================================================================
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information on table-data, waitinglist, etc.
+// ===============================================================================
+
+var notes = require("../db/notes.json");
+const fs = require("fs");
+
+
+// ===============================================================================
+// ROUTING
+// ===============================================================================
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  //--------------------------------------------------------------------------
+
+  app.get("/api/notes", (req, res) => {
+    //res.send('Get api notes called');
+    //Return notes
+    res.json(notes);
+  });
+
+  app.get("/api/notes/:id", (req, res) => {
+      //Find the note to get
+      const note = notes.find(c => c.id === parseInt("req.params.id"));
+      //404 for no note found
+      if (!note) return res.status(404).send('This note was not found');
+      
+  });
+
+  app.post('/api/notes', (req, res) => {
+      console.log(notes.length);
+      if (!req.body.title) res.status(404).send(`A new note is required`)
+      const note = {
+          id: notes.length + 1,
+          title: req.body.title,
+          text: req.body.text
+      };
+      console.log(note);
+      notes.push(note);
+      console.log(notes.length);
+      fs.writeFile("./db/notes.json", JSON.stringify(notes), function (err) {
+        if (err) throw err;
+        console.log("Writing to json file");
+        
+      });
+      res.send(note);
+  });
+  app.delete("/api/notes/:id", (req, res) => {
+    console.log("hello");
+    //Find the note to delete
+    const note = notes.find(c => c.id === parseInt("req.params.id"));
+    //404 for no note found
+    console.log(note);
+    
+    if (!note) return res.status(404).send('This note was not found');
+
+    const index = notes.indexOf(note.id);
+    //Remove note from selected index
+    notes.splice(index,1);
+    //Write to json file updated list of notes
+    fs.writeFile("./db/notes.json", JSON.stringify(notes), function (err) {
+        if (err) throw err;
+        console.log("Deleting note from json file");
+        res.end();
+      });
+    res.send(note);
+        
+
+});
+
+};
