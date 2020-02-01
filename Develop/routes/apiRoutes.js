0dd6db7cb5c5b6220cdebba9e8822fc7a91668e9
Tripp's Repo <tripp.parham@me.com>
2020-02-01 13:27:22 -0500

    Comments and ReadMe file has been updated.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,68 +1,56 @@
 // ===============================================================================
 // LOAD DATA
-// We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on table-data, waitinglist, etc.
-// ===============================================================================
-
+// Linking routes to a notes.json file "data" source.
+// Load fs package to write and read from the notes.json file.
+//================================================================================
 var notes = require("../db/notes.json");
 const fs = require("fs");
-
-
 // ===============================================================================
-// ROUTING
+// Module export of ROUTING functions
 // ===============================================================================
-
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  //--------------------------------------------------------------------------
-
+  // Get notes data from the notes.json file and post to screen.
   app.get("/api/notes", (req, res) => {
-    //res.send('Get api notes called');
     //Return notes
     res.json(notes);
   });
-
+  //Get the note of corresponding id to post to screen.
   app.get("/api/notes/:id", (req, res) => {
-      //Find the note to get
+      //Find the note to get and assign to note
       const note = notes.find(c => c.id === parseInt(req.params.id));
-      //404 for no note found
+      //Send 404 for no note found
       if (!note) return res.status(404).send('This note was not found');
-      
   });
-
+  //Post new note to the notes.json file.
   app.post('/api/notes', (req, res) => {
-      console.log(notes.length);
-      if (!req.body.title) res.status(404).send(`A new note is required`)
+      // Send error 404 if note is not entered at the time save button is hit.
+      if (!req.body.title) res.status(404).send(`A new note is required`);
+      // Assign note values to the new note
       const note = {
           id: notes.length + 1,
           title: req.body.title,
           text: req.body.text
       };
-      //console.log(note);
+      //Attach new note to the notes array.
       notes.push(note);
-      //console.log(notes.length);
+      //Rewrite the new notes list to list to notes.json
       fs.writeFile("./db/notes.json", JSON.stringify(notes), function (err) {
         if (err) throw err;
         console.log("Writing to json file");
-        
       });
       res.send(note);
   });
+  //Delete selected note from the notes.json file.
   app.delete("/api/notes/:id", (req, res) => {
-    //console.log("hello");
-    //console.log(notes);
     //Find the note to delete
     var note = notes.find(c => c.id === parseInt(req.params.id));
-    //404 for no note found
-    //console.log(note);
-   
-    console.log(note);
+    //Send 404 for no note found
     if (!note) return res.status(404).send('This note was not found');
-    console.log(note);
+    //Assign the index of the note to the variable index.
     const index = notes.indexOf(note);
-    console.log(index);
-    //Remove note from selected index
+    //Remove note from notes using the provided index
     notes.splice(index,1);
     //Write to json file updated list of notes
     fs.writeFile("./db/notes.json", JSON.stringify(notes), function (err) {
@@ -70,8 +58,6 @@ module.exports = function(app) {
         console.log("Deleting note from json file");
         res.end();
       });
-    res.send(note);
-        
+    res.send(note);  
 });
-
 };
--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -4,22 +4,15 @@
 // ===============================================================================
 var path = require("path");
 
-
-// ===============================================================================
-// ROUTING
-// ===============================================================================
-
 module.exports = function(app) {
   // HTML GET Requests
   // Below code handles when users "visit" a page.
   // In each of the below cases the user is shown an HTML page of content
-  // ---------------------------------------------------------------------------
-
+  // This function collect html code for the notes page.
   app.get("/api/notes", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/notes.html"));
   });
-
-  // If no matching route is found default to home
+  // If no matching page route is found, the default is set to home
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/index.html"));
   });
