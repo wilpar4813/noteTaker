101ebeb9a07ac583c817114538ad9c805f0e174e
Tripp's Repo <tripp.parham@me.com>
2020-02-04 18:13:23 -0500

    Update for Heroku

--- Develop/app.js
+++ Develop/app.js
@@ -27,6 +27,10 @@ app.use(express.json());
 require("./routes/apiRoutes")(app);
 require("./routes/htmlRoutes")(app);
 
+app.get("/", function(req, res) {
+    res.json(path.join(__dirname, "public/index.html"));
+  });
+  
 // =============================================================================
 // LISTENER
 // The below code effectively "starts" our server
